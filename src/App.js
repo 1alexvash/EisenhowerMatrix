2b2b75b689c0dfd928ea78719f2c038201d88518
Alexander <1alexvash@gmail.com>
2021-05-02 17:36:00 +0300

    Applied fonts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React from "react";
 import "./scss/main.css";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      Hello World!
+      <button>Hello</button>
+    </div>
+  );
 };
 
 export default App;
