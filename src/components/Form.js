7c35ffb96f9703e8faa87f36731205f12c3b3187
Alexander <1alexvash@gmail.com>
2021-05-02 18:13:14 +0300

    Added input handlers on form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,22 @@
 // TODO: probbably show this component in the pop up
 // TODO: add function to submit the data
 
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    importance: 5,
+    urgency: 5,
+  });
+
+  function updateInput(name, value) {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -13,18 +26,24 @@ const Form = () => {
   return (
     <form onSubmit={handleSubmit} className="Form">
       <label>Name of the task:</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={formValues.name}
+        onChange={(e) => updateInput("name", e.target.value)}
+      />
       <label>0 Not Important | 10 Very Important:</label>
       <input
         type="range"
-        onChange={(e) => console.log(e.target.value)}
+        value={formValues.importance}
+        onChange={(e) => updateInput("importance", parseInt(e.target.value))}
         min="1"
         max="10"
       />
       <label>0 Not Urgent | 10 Very Urgent:</label>
       <input
         type="range"
-        onChange={(e) => console.log(e.target.value)}
+        value={formValues.urgency}
+        onChange={(e) => updateInput("urgency", parseInt(e.target.value))}
         min="1"
         max="10"
       />
