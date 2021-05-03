b6df7dcd499e8ec4347d4d2f5f40835be33969f4
Alexander <1alexvash@gmail.com>
2021-05-03 18:14:54 +0300

    Saving tasks in the localStorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { createStore, action } from "easy-peasy";
 
 const store = createStore({
-  tasks: [],
+  tasks: localStorage.tasks === undefined ? [] : JSON.parse(localStorage.tasks),
   addTask: action((state, payload) => {
     state.tasks.push(payload);
+    localStorage.tasks = JSON.stringify(state.tasks);
   }),
 });
 
