068bea03bde6a340c80a8a11d8d03eeace1d73a1
Alexander <1alexvash@gmail.com>
2021-05-04 11:06:20 +0300

    Rendering dots on the map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,14 @@
+// TODO: add ability to click on the dot
+
 import React from "react";
+import { useStoreState } from "easy-peasy";
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const Map = () => {
+  const { tasks } = useStoreState((state) => state);
+  console.log("tasks:", tasks);
+
   return (
     <div className="Map">
       <div className="container">
@@ -12,6 +18,16 @@ const Map = () => {
         <div className="lines lines-vertical">
           {[numbers.map((n) => <div key={n} className="line" />)]}
         </div>
+        {tasks.map((task, index) => (
+          <div
+            key={index}
+            className="task"
+            style={{
+              bottom: `${(task.importance * 100) / 11 - 3.5}%`,
+              left: `${(task.urgency * 100) / 11}%`,
+            }}
+          ></div>
+        ))}
       </div>
       <div className="urgency">
         <div className="scale">
