9e52e75d0eb0c2e7355f9f587064e70c0a6383d3
Alexander <1alexvash@gmail.com>
2021-05-04 10:35:41 +0300

    Drawing a cell grid over the Map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,26 @@
 import React from "react";
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 const Map = () => {
-  return <div></div>;
+  return (
+    <div className="Map">
+      <div className="container">
+        <div className="horizontal-lines">
+          {[numbers.map((n) => <div key={n} className="line" />)]}
+        </div>
+        <div className="vertical-lines">
+          {[numbers.map((n) => <div key={n} className="line" />)]}
+        </div>
+      </div>
+      <div className="urgency">
+        <div className="scale">
+          {[numbers.map((number) => <span key={number}>{number}</span>)]}
+        </div>
+        <div className="text">Urgency</div>
+      </div>
+    </div>
+  );
 };
 
 export default Map;
