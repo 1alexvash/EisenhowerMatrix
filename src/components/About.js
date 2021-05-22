24fcc56b49f985ddd06cee39e3913e9f53b60d0f
Alexander <1alexvash@gmail.com>
2021-05-22 12:22:55 +0300

    Written about section text information

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import SchemeImage from "../images/scheme.png";
+
 const About = () => {
-  return <div>About page</div>;
+  return (
+    <div className="About">
+      <div className="text">
+        <p>
+          The aim of this application is to help you visualize your
+          tasks/projects, so you can prioritize them better and choose what{" "}
+          <b>really maters</b> and <b>makes</b> a difference.
+        </p>
+        <p>Look at the diagram ⏬ below to get the gist of how it works:</p>
+      </div>
+      <img className="image" src={SchemeImage} alt="" />
+    </div>
+  );
 };
 
 export default About;
